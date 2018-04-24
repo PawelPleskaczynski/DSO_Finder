6f38429f3a59d376da32e566a7d9396ea86c3d46
PawelPleskaczynski <hamstersztyk@gmail.com>
2018-04-24 17:38:38 +0200

    1.8

--- object.js
+++ object.js
@@ -425,7 +425,7 @@ function openDSO() {
 
 
 function ra_plus() {
-  ra = ra + 0.1;
+  ra = Number(ra) + 0.1;
 
   jQuery("#loading_bg").show();
   image.onload = function () {
@@ -447,7 +447,7 @@ function ra_plus() {
 }
 
 function ra_minus() {
-  ra = ra - 0.1;
+  ra = Number(ra) - 0.1;
 
   jQuery("#loading_bg").show();
   image.onload = function () {
@@ -469,7 +469,7 @@ function ra_minus() {
 }
 
 function dec_plus() {
-  dec = dec + 0.1;
+  dec = Number(dec) + 0.1;
 
   jQuery("#loading_bg").show();
   image.onload = function () {
@@ -491,7 +491,7 @@ function dec_plus() {
 }
 
 function dec_minus() {
-  dec = dec - 0.1;
+  dec = Number(dec) - 0.1;
 
   jQuery("#loading_bg").show();
   image.onload = function () {
