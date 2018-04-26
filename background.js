d32d1853b0fd566dd2399660dbd6a3668b540788
PawelPleskaczynski <hamstersztyk@gmail.com>
2018-04-26 17:44:45 +0200

    2.2

---
+++ background.js
@@ -0,0 +1,29 @@
+var picArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+var random = Math.floor((Math.random() * 10) + 1);
+window.onLoad = setbg();
+
+function setbg() {
+  if (!isNaN(picArr[random])) {
+    var tempimg = new Image();
+    tempimg.src = "images/" + picArr[random] + ".jpg";
+    tempimg.onload = function(){
+      jQuery("body").css("background","url(images/" + picArr[random] + ".jpg) no-repeat center center fixed");
+      jQuery(".bg").fadeOut("slow");
+      document.body.style.webkitBackgroundSize = "cover";
+      document.body.style.mozBackgroundSize = "cover";
+      document.body.style.oBackgroundSize = "cover";
+      document.body.style.backgroundSize = "cover";
+    };
+  } else {
+    var tempimg = new Image();
+    tempimg.src = "images/" + picArr[random] + ".jpg";
+    tempimg.onload = function(){
+      jQuery("body").css("background","url(images/6.jpg) no-repeat center center fixed");
+      jQuery(".bg").fadeOut("slow");
+      document.body.style.webkitBackgroundSize = "cover";
+      document.body.style.mozBackgroundSize = "cover";
+      document.body.style.oBackgroundSize = "cover";
+      document.body.style.backgroundSize = "cover";
+    };
+  }
+}
